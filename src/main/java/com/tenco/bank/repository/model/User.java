dc58a489c9c8635f640696259a76dacc8ed3f43d
thxtdy <thjjang0409@gmail.com>
2024-08-05 11:20:14 +0900

    기능 틀 잡기

---
+++ src/main/java/com/tenco/bank/repository/model/User.java
@@ -0,0 +1,56 @@
+package com.tenco.bank.repository.model;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class User {
+	private Integer id;
+	private String userName;
+	private String password;
+	private Timestamp fullName;
+	private Timestamp createdAt;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Timestamp getFullName() {
+		return fullName;
+	}
+	public void setFullName(Timestamp fullName) {
+		this.fullName = fullName;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	
+}
