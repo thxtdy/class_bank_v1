601c58bc452a51d6694b61d97ca64a080c418da6
thxtdy <thjjang0409@gmail.com>
2024-08-06 14:23:08 +0900

    mapper 생성 및 추가

--- src/main/java/com/tenco/bank/repository/model/Account.java
+++ src/main/java/com/tenco/bank/repository/model/Account.java
@@ -7,25 +7,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
 public class Account {
-	
+
 	private Integer id;
 	private String number;
 	private String password;
 	private Long balance;
 	private Integer userId;
 	private Timestamp createdAt;
-	
+
 	// 출금 기능
 	public void withdraw(Long amount) {
 		this.balance -= amount;
 	}
-	
+
 	public void deposit(Long amount) {
 		this.balance += amount;
 	}
@@ -33,5 +34,5 @@ public class Account {
 	// 페스워드 체크
 	// 잔액 여부 확인
 	// 계좌 소유자 확인 가능
-	
+
 }
